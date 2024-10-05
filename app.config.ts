a7e312af7f2cde27f50c1a05b329363b9ecf35e7
Neil <10043293+Neil-Lin@users.noreply.github.com>
2024-10-05 21:17:09 +0800

    5 changes by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   // Configure Content Wind here
-  cover: '/cover.jpg',
+  cover: '/social-image.png',
   socials: {
     twitter: '',
     github: 'https://github.com/Neil-Lin',
